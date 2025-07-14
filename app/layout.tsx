986b244833fd08c4c309cb80409189a0005c21bf
Narayan Poudel <narayanpoudel@Narayans-MacBook-Pro.local>
2025-07-15 00:35:44 +0545

    starting with project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
+
+const IbmPlexSans = IBM_Plex_Sans({
+  display: "swap",
+  subsets: ["latin"],
+  weight: ["500", "600", "700"],
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${IbmPlexSans.className} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
